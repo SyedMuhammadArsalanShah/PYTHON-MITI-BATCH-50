490fd2a1b97dcfaa1f2437963bc644b2fbf5e306
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2026-07-09 18:36:04 +0500

    code

---
+++ Lecture01Variables/main.py
@@ -0,0 +1,16 @@
+print("Bismillah")
+
+
+print("Welcome to MITI")
+
+userName="Siddique"
+userAge="17"
+user_Course="Python"
+timing="5to7"
+github=""
+
+
+print("User Name  :",userName)
+print( "User Age :",  userAge)
+print( "User Course  :",  user_Course)
+print( "User Course Timing :",  timing)
